766873286a7cf14984ff6e69463babcf6fb82245
Michael Alexis <99289708+MGato24@users.noreply.github.com>
2022-02-21 16:40:19 -0500

    Add files via upload

---
+++ RMarkdown/script.R
@@ -0,0 +1,10 @@
+
+
+# HACIENDO UN DOCUMENTO EN RMARKDOWN --------------------------------------
+
+
+# YAML: -------------------------------------------------------------------
+
+#Controla las configuraciones de todo el documento
+
+
